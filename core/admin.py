9cbc50867e2273be34c5e5b8ecbb3450e2a56291
yasmin marcelino <yasmarcelino91@gmail.com>
2022-06-30 15:06:25 -0300

    commit Instalação do black

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,9 @@
+from core.models import Marca
 from django.contrib import admin
 
 from core.models import Categoria
 
 admin.site.register(Categoria)
 
-from core.models import Marca
 
 admin.site.register(Marca)
